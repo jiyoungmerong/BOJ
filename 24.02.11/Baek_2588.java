6cd644f0db401df40cd27b84f6543bb0f564ce7f
jiyoungmerong <jiyoungmerong@gmail.com>
2024-02-11 20:02:39 +0900

    2588 곱셈

---
+++ 24.02.11/Baek_2588.java
@@ -0,0 +1,38 @@
+/*
+곱셈
+
+문제
+(세 자리 수) × (세 자리 수)는 다음과 같은 과정을 통하여 이루어진다.
+
+(1)과 (2)위치에 들어갈 세 자리 자연수가 주어질 때 (3), (4), (5), (6)위치에 들어갈 값을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 (1)의 위치에 들어갈 세 자리 자연수가, 둘째 줄에 (2)의 위치에 들어갈 세자리 자연수가 주어진다.
+
+출력
+첫째 줄부터 넷째 줄까지 차례대로 (3), (4), (5), (6)에 들어갈 값을 출력한다.
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Baek_2588 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        int result1 = a * (b % 10);
+        int result2 = a * ((b % 100) / 10);
+        int result3 = a * (b / 100);
+        int result4 = a * b;
+
+        bw.write(result1 + "\n" + result2 + "\n" + result3 + "\n" + result4 + "\n");
+        bw.flush();
+    }
+}
