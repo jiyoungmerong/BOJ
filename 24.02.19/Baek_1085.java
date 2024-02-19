0235a01256cf246564267517373ef84f2f856e7c
jiyoungmerong <jiyoungmerong@gmail.com>
2024-02-19 22:54:46 +0900

    1085 직사각형에서 탈출

---
+++ 24.02.19/Baek_1085.java
@@ -0,0 +1,39 @@
+/*
+직사각형에서 탈출
+
+문제
+한수는 지금 (x, y)에 있다. 직사각형은 각 변이 좌표축에 평행하고, 왼쪽 아래 꼭짓점은 (0, 0), 오른쪽 위 꼭짓점은 (w, h)에 있다.
+직사각형의 경계선까지 가는 거리의 최솟값을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 x, y, w, h가 주어진다.
+
+출력
+첫째 줄에 문제의 정답을 출력한다.
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Baek_1085 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int x_min = Math.min(x, w - x);
+        int y_min = Math.min(y, h - y);
+
+        bw.write(String.valueOf(Math.min(x_min, y_min)));
+        bw.flush();
+    }
+}
